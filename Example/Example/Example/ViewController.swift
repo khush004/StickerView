2a23b734fa40e724b38b99dc5017ca572868be86
Khushbu Desai <khushbu@varshaawebteam.com>
2017-10-25 17:25:25 +0530

    First Commit with changes

---
+++ Example/Example/Example/ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by 刘业臻 on 16/4/23.
+//  Copyright © 2016年 luiyezheng. All rights reserved.
+//
+
+import UIKit
+import JLStickerTextView
+
+class ViewController: UIViewController {
+    var colorsArray = [UIColor.white, UIColor.black, UIColor.yellow]
+    var fontNamesArray = ["AcademyEngravedLetPlain", "AlNile-Bold", "Chalkduster"]
+    var textAlphaArray = [0.3, 0.6, 1.0]
+    var lineSpacings = [1,30,50]
+    var shadowOffsets = [1.0, 10.0, 20.0]
+    
+    @IBOutlet var stickerView: JLStickerImageView!
+    
+    @IBAction func onRefreshColor(_ sender: UIBarButtonItem) {
+        let index = arc4random_uniform(3)
+        let color = colorsArray[Int(index)]
+        
+        stickerView.textColor = color
+    }
+    
+    @IBAction func onRefreshLineSpacing(_ sender: UIBarButtonItem) {
+        let index = arc4random_uniform(3)
+        let lineSpacing = lineSpacings[Int(index)]
+        
+        stickerView.lineSpacing = CGFloat(lineSpacing)
+    }
+    
+    
+    @IBAction func onRefreshFont(_ sender: UIBarButtonItem) {
+        let index = arc4random_uniform(3)
+        let fontName = fontNamesArray[Int(index)]
+        
+        stickerView.fontName = fontName
+    }
+    
+    
+    @IBAction func onRefreshTextAlpha(_ sender: UIBarButtonItem) {
+        let index = arc4random_uniform(3)
+        let textAlpha = textAlphaArray[Int(index)]
+        
+        stickerView.textAlpha = CGFloat(textAlpha)
+        
+    }
+    
+    @IBAction func onAddLabel(_ sender: UIBarButtonItem) {
+        //Add the label
+        stickerView.addLabel()
+        
+        //Modify the Label
+        stickerView.textColor = UIColor.white
+        stickerView.textAlpha = 1
+        stickerView.currentlyEditingLabel.closeView!.image = UIImage(named: "cancel")
+        stickerView.currentlyEditingLabel.rotateView?.image = UIImage(named: "rotate")
+        stickerView.currentlyEditingLabel.border?.strokeColor = UIColor.white.cgColor
+        
+    }
+    
+    @IBAction func onRefreshShadow(_ sender: UIBarButtonItem) {
+        let index = arc4random_uniform(3)
+        let shadowOffset = shadowOffsets[Int(index)]
+        
+        stickerView.textShadowOffset = CGSize(width: CGFloat(shadowOffset), height: 10)
+        stickerView.textShadowColor = UIColor.red
+    }
+    
+    
+    @IBAction func onSaveImage(_ sender: UIBarButtonItem) {
+        //render text on Image and save the Image
+        let image = stickerView.renderTextOnView(stickerView)
+        UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
+
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //This is optional. Not tested yet
+        stickerView.limitImageViewToSuperView()
+        stickerView.center = self.view.center
+        stickerView.contentMode = .scaleAspectFit
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ JLStickerTextView.podspec
@@ -0,0 +1,25 @@
+
+
+Pod::Spec.new do |s|
+
+  s.name         = "JLStickerTextView"
+  s.version      = "0.1.1"
+  s.summary      = "add text(multiple line support) to imageView, edit, rotate or resize them as you want, then render the text on image"
+
+  s.homepage     = "https://github.com/luiyezheng/JLStickerTextView"
+
+  s.license      = { :type => 'MIT', :file => 'LICENSE' }
+
+  s.author       = { "luiyezheng" => "luiyezheng@foxmail.com" }
+
+  s.source       = { :git => "https://github.com/luiyezheng/JLStickerTextView.git", :tag => "0.1.1" }
+
+  s.platform = :ios, "8.0"
+
+  s.source_files  = "Source"
+
+  s.requires_arc = false
+
+  s.frameworks = "UIKit"
+
+end
